d3433aa51f4d426d443af8b3ccd2feaf68e9ec7b
Aunggoon Arpasunun <aunggoon.arpa@kkpfg.com>
2024-11-26 16:33:44 +0700

    fix hex hash sha512

--- src/utils/encryption.rs
+++ src/utils/encryption.rs
@@ -1,16 +1,14 @@
-use base64ct::{Base64, Encoding};
 use sha2::{Digest, Sha512};
 
-const BUF_SIZE: usize = 128;
+const BUF_SIZE: usize = 512;
 
 pub async fn hash_password(password: String) -> Option<String> {
+    if password.is_empty() {
+        return None;
+    }
+
     let mut hasher = Sha512::new();
     hasher.update(password.as_bytes());
     let hash = hasher.finalize();
-    let mut enc_buf = [0u8; BUF_SIZE];
-    let result = Base64::encode(&hash, &mut enc_buf);
-    match result {
-        Ok(data) => Some(String::from(data)),
-        Err(_err) => None
-    }
+    Some(hex::encode(hash))
 }
